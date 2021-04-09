6aaa1889ed0decad463a6fc390cf14c9fafde485
Christian Khan <christiankhan@Hotmail.com>
2021-04-09 19:58:16 -0400

    custom error handling + mongoose validation

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+const {isEmail} = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String, 
-        required: true, 
+        required: [true, 'Please enter an email'], 
         unique: true, 
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail,  'Email address not valid']
     }, 
     password: {
         type: String, 
-        required: true, 
-        minLenth: 6
+        required: [true, 'Please enter a password'], 
+        minlength: [6, 'Password must be atleast 6 characters long']
     }, 
 }); 
 
