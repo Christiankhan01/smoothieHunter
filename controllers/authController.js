d454d93ee52e1f9b38e3e0fc0eaaf4aa3ad6fba4
Christian Khan <christiankhan@Hotmail.com>
2021-04-09 18:55:11 -0400

    userSchema added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
   }
@@ -9,12 +11,18 @@ module.exports.signup_get = (req, res) => {
   module.exports.signup_post = async (req, res) => {
     const {email, password} = req.body; 
     
-    console.log(email, password); 
-    res.send('new signup');
+    try {
+        const user = await User.create({email, password})
+        res.status(201).json(user); 
+    } catch (err) {
+        console.log(err.message); 
+        res.status(400).send('Error, User not created')
+    }
   }
   
   module.exports.login_post = async (req, res) => {
     const {email, password} = req.body; 
+    
     
     console.log(email, password); 
     res.send('user login');
