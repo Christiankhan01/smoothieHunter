02e9411a4b372ac922834bc1f5497ba8d9ecb1d2
Christian Khan <christiankhan@Hotmail.com>
2021-04-12 16:47:35 -0400

    verify jwt and res with user

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const {requireAuth} = require('./middleware/authMiddleware');
+const {requireAuth, checkUser} = require('./middleware/authMiddleware');
 
 
 
@@ -25,6 +25,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies',requireAuth,  (req, res) => res.render('smoothies'));
 app.use(authRoutes);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -27,4 +27,36 @@ const requireAuth = (req, res, next) => {
     res.redirect('/login');    
     }
 }
-module.exports = {requireAuth}; 
+/**
+ * 1. check if token exists...
+ * ... if so, verify token. If err when verifying... 
+ * ... set res.local to null & go next()...
+ * ... else find user by Id and set res.local to user. 
+ * @param {*} req   send cookies.jwt to be verified
+ * @param {*} res   response of null OR user
+ * @param {*} next 
+ */
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt; 
+    if(token) {
+        jwt.verify(token, process.env.jwtSecret, async (err, decodedToken) => {
+            if(err) {
+                console.log(err.message); 
+                res.locals.user = null; 
+                next(); 
+            }
+            else{
+                console.log(decodedToken); 
+                let user = await User.findById(decodedToken.id); 
+                res.locals.user = user;  
+                next(); 
+            }
+        });
+    }
+    else{
+        res.locals.user = null; 
+        next(); 
+    }
+    
+}
+module.exports = {requireAuth, checkUser}; 
